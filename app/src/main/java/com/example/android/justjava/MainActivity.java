519173556731210bf17bd815d23b98f26275ca9b
arturo <whyirun@gmail.com>
2015-07-12 19:50:50 -0700

    Finished

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -35,14 +38,33 @@ public class MainActivity extends ActionBarActivity {
         EditText text = (EditText) findViewById(R.id.name_field);
         String value = text.getText().toString();
 
-        int price = calculatePrice();
-        displayMessage(createOrderSummary(price, hasWhippedCream, hasChocolateChip, value));
+        int price = calculatePrice(hasWhippedCream, hasChocolateChip);
+
+        String message = createOrderSummary(price, hasWhippedCream, hasChocolateChip, value);
+
+        // Use an intent to launch an email app.
+        // Send the order summary in the email body.
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "JustJava order for " + value);
+        intent.putExtra(Intent.EXTRA_TEXT, message );
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
      * This method is called when the increment button is clicked./
      */
     public void increment(View view) {
+        if (quantity > 100) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
+
 
         quantity = quantity + 1;
         displayQuantity(quantity);
@@ -52,6 +74,12 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the decrement button is clicked.
      */
     public void decrement(View view) {
+        if (quantity <= 1) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
 
         quantity = quantity - 1;
         displayQuantity(quantity);
@@ -72,7 +100,6 @@ public class MainActivity extends ActionBarActivity {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
-    */
 
 
     private void displayMessage(String message) {
@@ -80,27 +107,39 @@ public class MainActivity extends ActionBarActivity {
         orderSummaryTextView.setText(message);
 
     }
+     */
 
-        /**
+
+    /**
          * Calculates the price of the order based on the current quantity.
          *
          * @return the price
          */
 
-    private int calculatePrice() {
-        int price = quantity * 5;
+    private int calculatePrice(boolean hasWhippedCream, boolean hasChocolateChip) {
+        int basePrice = 5;
+        if(hasWhippedCream){
+            basePrice = basePrice + 1;
+        }
+
+        if(hasChocolateChip){
+            basePrice = basePrice + 2;
+        }
+
+
+        int price = quantity * basePrice;
         return price;
     }
 
     private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolateChip, String value){
-        //String cname = "James";
-        //int price = calculatePrice();
+
+
 
         String summaryMessage = "Name: " + value ;
         summaryMessage += "\nAdd Whipped Cream? "+ hasWhippedCream;
         summaryMessage += "\nAdd Chocolate Chip? "+ hasChocolateChip;
         summaryMessage += "\nQuantity: " + quantity;
-        summaryMessage += "\nTotal: $" + price;
+        summaryMessage += "\nTotal: $" + price ;
         summaryMessage += "\nThank you! ";
         return summaryMessage;
 
